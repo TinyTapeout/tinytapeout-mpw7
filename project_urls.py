a06749dd93bd230c9cea13a027443885f61ce50d
Uri Shaked <uri@urishaked.com>
2022-08-09 11:52:24 +0300

    add tiny-tapeout-test project
    just a simple clock divider with a shift register animating a loop on the 7-segment display

--- project_urls.py
+++ project_urls.py
@@ -4,4 +4,5 @@ urls = [
             'https://github.com/mattvenn/wokwi-verilog-gds-test',
             'https://github.com/mattvenn/animation_tinytapeout_demo',
             'https://github.com/mattvenn/wokwi_inverters',
+            'https://github.com/wokwi/tiny-tapeout-test-simple',
     ]
