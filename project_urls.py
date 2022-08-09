d6681a20c4f66384fa94f3226c4f72c68696ea0d
Matt Venn <matt@mattvenn.net>
2022-08-09 10:35:03 +0200

    add filler as first project

--- project_urls.py
+++ project_urls.py
@@ -1,5 +1,6 @@
 # name must be valid verilog module name set in scan_wrapper.v
 urls = [
+            'https://github.com/mattvenn/wokwi_filler',
             'https://github.com/mattvenn/wokwi-verilog-gds-test',
             'https://github.com/mattvenn/animation_tinytapeout_demo',
             'https://github.com/mattvenn/wokwi_inverters',
