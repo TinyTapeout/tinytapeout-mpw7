bf0f49f14560bf25d276c25d00ab70c1f9e57cab
matt venn <matt@mattvenn.net>
2022-08-13 16:01:49 +0200

    add 7 seg decoder

--- project_urls.py
+++ project_urls.py
@@ -5,4 +5,5 @@ project_urls = [
             'https://github.com/mattvenn/wokwi_inverters',
             'https://github.com/wokwi/tiny-tapeout-test-simple',
             'https://github.com/omerk/tinytapeout-demo1',
+            'https://github.com/mattvenn/tinytapeout-7seg-decoder',
     ]
