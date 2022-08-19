440b54bcefc7787477e86cf0bcf45db7ab7aa734
matt venn <matt@mattvenn.net>
2022-08-19 16:05:21 +0200

    Update project_urls.py

--- project_urls.py
+++ project_urls.py
@@ -9,4 +9,5 @@ project_urls = [
             'https://github.com/omerk/tinytapeout-verilog-test',
             'https://github.com/gregdavill/tinytapeout_spin0',
             'https://github.com/mole99/wokwi-1bit-alu',
+            'https://github.com/ericsmi/tinytapeout_popcnt.git',
     ]
