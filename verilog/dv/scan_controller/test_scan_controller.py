a1e19b611a35321d5434e1306f78c48f6b359436
matt venn <matt@mattvenn.net>
2022-08-26 16:02:03 +0200

    working tb for 7 seg counter

--- verilog/dv/scan_controller/test_scan_controller.py
+++ verilog/dv/scan_controller/test_scan_controller.py
@@ -3,35 +3,35 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
 from scan_wrapper import *
 
-NUM_DESIGNS = 498
-
 # utility functions. using 2nd bit as reset and 1st bit as clock for synchronous design examples
 async def reset(dut):
+    for i in range(2):
+        await RisingEdge(dut.ready);
+        dut.inputs.value = 0b10
+        await RisingEdge(dut.ready);
+        dut.inputs.value = 0b11
     await RisingEdge(dut.ready);
-    dut.inputs = 0b10
-    await RisingEdge(dut.ready);
-    dut.inputs = 0b11
-    await RisingEdge(dut.ready);
-    dut.inputs = 0b0
+    dut.inputs.value = 0b0
 
 async def single_cycle(dut):
     await RisingEdge(dut.ready);
-    dut.inputs = 0b1
+    dut.inputs.value = 0b1
     await RisingEdge(dut.ready);
-    dut.inputs = 0b0
+    dut.inputs.value = 0b0
 
 @cocotb.test()
-async def test_lesson_1(dut):
+async def test(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.fork(clock.start())
 
-    dut.reset = 1
-    dut.active_select = 12 # 7 seg seconds
+    dut.reset.value = 1
+    dut.active_select.value = 12 # 7 seg seconds
     await ClockCycles(dut.clk, 10)
-    dut.reset = 0
+    dut.reset.value = 0
 
-    dut.inputs = 0x00
-    for i in range(10):
+    await reset(dut)
+
+    dut.inputs.value = 0x00
+    for i in range(15):
         print("clock {}".format(i))
-        await RisingEdge(dut.ready);
-
+        await single_cycle(dut)
--- verilog/dv/scan_controller/test_scan_controller_tb.v
+++ verilog/dv/scan_controller/test_scan_controller_tb.v
@@ -1,4 +1,6 @@
 `default_nettype none
+`define UNIT_DELAY #1
+`define FUNCTIONAL
 `include "libs.ref/sky130_fd_sc_hd/verilog/primitives.v"
 `include "libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v"
 
